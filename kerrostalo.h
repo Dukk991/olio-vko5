241fbe22956cc9fe31bfc8f0df1fb6be36c81d6e
Valtteri K.A.P <144036143+Dukk991@users.noreply.github.com>
2024-02-13 14:13:23 +0200

    Create kerrostalo.h

---
+++ kerrostalo.h
@@ -0,0 +1,21 @@
+#ifndef KERROSTALO_H
+#define KERROSTALO_H
+
+#include "asunto.h"
+#include "kerros.h"
+#include "katutaso.h"
+
+class Kerrostalo
+{
+public:
+    Kerrostalo();
+    ~Kerrostalo();
+    double laskeKulutus(double);
+
+private:
+    Katutaso *eka;
+    Kerros *toka;
+    Kerros *kolmas;
+};
+
+#endif //KERROSTALO.H
